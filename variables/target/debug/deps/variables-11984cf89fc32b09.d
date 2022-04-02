43df23fa9b4eec762fa37aece225522c4405086d
Benni-Math <benediktjens.arnarsson@gmail.com>
2022-04-02 18:11:56 -0400

    Finished variables section

---
+++ variables/target/debug/deps/variables-11984cf89fc32b09.d
@@ -0,0 +1,5 @@
+c:\Users\bened\projects\variables\target\debug\deps\variables-11984cf89fc32b09.rmeta: src\main.rs
+
+c:\Users\bened\projects\variables\target\debug\deps\variables-11984cf89fc32b09.d: src\main.rs
+
+src\main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+C:\Users\bened\projects\variables\target\debug\variables.exe: C:\Users\bened\projects\variables\src\main.rs
