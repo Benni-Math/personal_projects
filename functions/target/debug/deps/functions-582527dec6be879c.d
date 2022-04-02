13699e34a0467b8200338d56d321b8020602c839
Benni-Math <benediktjens.arnarsson@gmail.com>
2022-04-02 18:22:33 -0400

    Getting started with functions

---
+++ functions/target/debug/deps/functions-582527dec6be879c.d
@@ -0,0 +1,5 @@
+c:\Users\bened\projects\functions\target\debug\deps\functions-582527dec6be879c.rmeta: src\main.rs
+
+c:\Users\bened\projects\functions\target\debug\deps\functions-582527dec6be879c.d: src\main.rs
+
+src\main.rs:
---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+C:\Users\bened\projects\functions\target\debug\functions.exe: C:\Users\bened\projects\functions\src\main.rs
