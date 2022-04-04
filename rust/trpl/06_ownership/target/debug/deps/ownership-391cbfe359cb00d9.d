a37f21757664cd402face872ee0438306f829d0c
Benni-Math <benediktjens.arnarsson@gmail.com>
2022-04-04 16:56:39 -0400

    Finished Ownership chapter

--- rust/trpl/06_ownership/target/debug/deps/ownership-391cbfe359cb00d9.d
+++ rust/trpl/06_ownership/target/debug/deps/ownership-391cbfe359cb00d9.d
@@ -1,5 +1,5 @@
-c:\Users\bened\projects\rust\trpl\ownership\target\debug\deps\ownership-391cbfe359cb00d9.rmeta: src\main.rs
+c:\Users\bened\projects\rust\trpl\06_ownership\target\debug\deps\ownership-391cbfe359cb00d9.rmeta: src\main.rs
 
-c:\Users\bened\projects\rust\trpl\ownership\target\debug\deps\ownership-391cbfe359cb00d9.d: src\main.rs
+c:\Users\bened\projects\rust\trpl\06_ownership\target\debug\deps\ownership-391cbfe359cb00d9.d: src\main.rs
 
 src\main.rs:
