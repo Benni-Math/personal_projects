9534c67db368448b2488f4dd8a179da37d7802e5
Benni-Math <benediktjens.arnarsson@gmail.com>
2022-04-04 17:02:05 -0400

    Starting structs

---
+++ rust/trpl/07_structs/target/debug/deps/structs-f236a4d9ee5a6503.d
@@ -0,0 +1,5 @@
+c:\Users\bened\projects\rust\trpl\07_structs\target\debug\deps\structs-f236a4d9ee5a6503.rmeta: src\main.rs
+
+c:\Users\bened\projects\rust\trpl\07_structs\target\debug\deps\structs-f236a4d9ee5a6503.d: src\main.rs
+
+src\main.rs:
