3ded19e58193b69e161f320047e1624b6c285886
Benni-Math <benediktjens.arnarsson@gmail.com>
2022-04-10 22:59:08 -0400

    Starting the common collection chapter

---
+++ rust/trpl/10_common_collections/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let mut v: Vec<i32> = Vec::new();
+    // using the vec! macro
+    // let v  = vec![1, 2, 3]; 
+
+    // adding new elements
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
+}
