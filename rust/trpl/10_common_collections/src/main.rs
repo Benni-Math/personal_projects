958bf3019993a0a13748288cad0d76e75170d01d
Benni-Math <benediktjens.arnarsson@gmail.com>
2022-04-18 11:46:40 -0400

    Finished exercises

--- rust/trpl/10_common_collections/src/main.rs
+++ rust/trpl/10_common_collections/src/main.rs
@@ -59,14 +59,63 @@ fn _vec_borrowing() {
 
 // Exercises
 
-fn _median_mode() {
+fn _median_mode(list: &[i32]) -> (i32, i32) {
+    let mut v = list.to_vec();
+    v.sort();
 
+    // Calculating median via sorting
+    let length = v.len();
+    let mid: usize;
+    if length%2==0 {
+        mid = length/2;
+    } else {
+        mid = (length+1)/2;
+    }
+    let median = v[mid];
+
+    // Calculating the mode using a HashMap
+    let mut map = HashMap::new();
+    let mut max = 0;
+    let mut mode = v[0];
+    for num in v {
+        let count = map.entry(num).or_insert(0);
+        *count += 1;
+        if *count > max {
+            max = *count;
+            mode = num;
+        }
+    }
+
+    (median, mode)
 }
 
-fn _pig_latin() {
-
-}
-
-fn _add_to_department() {
+fn _pig_latin(entry: &String) ->  Option<String> {
+    fn is_vowel(c: Option<char>) -> bool {
+        let vowels = ['a', 'e', 'i', 'o', 'u'];
+        match c {
+            Some(character) => {
+                for vowel in vowels {
+                    if character==vowel {
+                        return true;
+                    }
+                }
+                false
+            },
+            None => false,
+        }
+        
+    }
+    // For this implementation, I'm assuming that all inputted values us the Latin alphabet
+    if entry.chars().all(char::is_alphabetic) {
+        let encoded: String;
+        if is_vowel(entry.chars().next()) {
+            encoded = format!("{}-hay", entry);
+        } else {
+            encoded = format!("{}-{}ay", &entry[1..], entry.chars().next().unwrap());
+        }
+        Some(encoded)
+    } else {
+        None
+    }
     
 }
