eb9089f9c2c59cd241e6d6e3787d0ef3c1c302b4
Benni-Math <benediktjens.arnarsson@gmail.com>
2022-05-15 16:20:08 -0400

    Rust - First CLI (#6)
    * First part of binary tree implementation
    
    * Fixing imports
    
    * Finished the first command line project
    
    * Cleaning up

--- rust/fun_with_trees/src/lib.rs
+++ rust/fun_with_trees/src/lib.rs
@@ -1,10 +1,19 @@
-pub mod first;
+mod tree;
+pub use crate::tree::Tree;
 
 #[cfg(test)]
 mod tests {
+    use crate::Tree;
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn works_build_tree() {
+        let mut tree = Tree::new();
+        tree.insert(8);
+        tree.insert(10);
+        tree.insert(3);
+        tree.insert(1);
+        tree.insert(6);
+
+        assert_eq!(tree.root.is_some(), true);
+        println!("{:?}", tree);
     }
-}
+}  
