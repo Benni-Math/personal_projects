e7fce3505b52ebca8ca3c0447e9abc0751aedb0c
Benni-Math <benediktjens.arnarsson@gmail.com>
2022-05-16 19:43:11 -0400

    Finished functional programming features

--- rust/trpl/13_functional_features/src/lib.rs
+++ rust/trpl/13_functional_features/src/lib.rs
@@ -1,5 +1,5 @@
 mod cache_tools;
-
+mod iterators;
 
 use std::thread;
 use std::time::Duration;
@@ -54,3 +54,44 @@ pub fn generate_workout_rc(intensity: u32, random_number: u32) {
 
 
 //Iterators:
+// pub trait Iterator {
+//     type Item;
+
+//     fn next(&mut self) -> Option<Self::Item>;
+// }
+use std::io;
+use std::error::Error;
+use iterators::shoe::{ Shoe, shoes_in_size };
+
+pub fn filtering_shoes() -> Result<(), Box<dyn Error>> {
+    let available_shoes = vec![
+        Shoe {
+            size: 8,
+            style: String::from("sandals"),
+        },
+        Shoe {
+            size: 8,
+            style: String::from("Gucci")
+        },
+    ];
+
+    println!("Enter your shoe size:");
+
+    let mut shoe_size = String::new();
+
+    io::stdin()
+        .read_line(&mut shoe_size)
+        .expect("Failed to read line");
+    
+    let shoe_size: u32 = shoe_size.trim().parse()?;
+
+    let matching_shoes = shoes_in_size(available_shoes, shoe_size);
+
+    if matching_shoes.len() < 1 {
+        println!("Sorry, we have no shoes in your size...");
+    } else {
+        println!("Here are the available shoes in your size:\n {:?}", matching_shoes);
+    }
+
+    Ok(())
+}
