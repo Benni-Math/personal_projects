b4acae3f9e2d283a8456fd4e6959523d1a2a4be3
Benni-Math <benediktjens.arnarsson@gmail.com>
2022-06-28 16:08:15 -0400

    Finished Chapter 1 of GOPL

--- go/gopl/ch01/fetch.go
+++ go/gopl/ch01/fetch.go
@@ -21,6 +21,7 @@ func main() {
         fmt.Printf("Status: %s\n", resp.Status)
 
         _, err = io.Copy(os.Stdout, resp.Body)
+        resp.Body.Close() // don't leak resources
         if err != nil {
             fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
             os.Exit(1)
