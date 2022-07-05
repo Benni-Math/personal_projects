6667862c6ee20fba25d4bd6007e693d76d6e3a73
Benni-Math <benediktjens.arnarsson@gmail.com>
2022-07-05 13:55:45 -0400

    Implimented malloc a la K&R -- will upgrade and look over it again later

--- c-assembly/util/k-and-r/malloc.c
+++ c-assembly/util/k-and-r/malloc.c
@@ -45,3 +45,50 @@ void *malloc(unsigned nbytes) {
                 return NULL; // none left
     }
 }
+
+#define NALLOC 1024
+
+/**
+* morecore: ask system for more memory
+*/
+static Header *morecore(unsigned nu) {
+    char *cp, *sbrk(int);
+    Header *up;
+
+    if (nu < NALLOC)
+        nu = NALLOC;
+    cp = sbrk(nu * sizeof(Header));
+    if (cp == (char *) -1) // no OS memory left
+        return NULL
+    up = (Header *) cp;
+    up->s.size = nu;
+    free((void *)(up+1));
+    return freep;
+}
+
+/**
+*   free: put block ap in free list
+*/
+void free(void *ap) {
+    Header *bp, *p;
+
+    bp = (Header *)ap - 1; // point to block header
+    for (p = freep; !(bp > p && bp < p->s.ptr); p = p->s.ptr)
+        if (p >= p->s.ptr && (bp > p || bp < p->s.ptr))
+            break; // freed block at start or end of arena
+
+    if (bp + bp->s.size == p->s.ptr) { // join to upper nbr
+        bp->s.size += p->s.ptr->s.size;
+        bp->s.ptr = p->s.ptr->s.ptr;
+    } else {
+        bp->s.ptr = p->s.ptr;
+    }
+    if (p + p->s.size == bp) { // join to lower nbr
+        p->s.size += bp->s.size;
+        p->s.ptr = bp->s.ptr;
+    } else {
+        p->s.ptr = bp;
+    }
+    freep = p;
+}
+
