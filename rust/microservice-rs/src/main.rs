2552e8c6eeb33b7a74ecf433f4a05f8ce25240b6
Benni-Math <benediktjens.arnarsson@gmail.com>
2022-07-15 10:56:45 -0400

    Starting work on a Rust microservice mini-project

---
+++ rust/microservice-rs/src/main.rs
@@ -0,0 +1,20 @@
+use hyper::server::{Request, Response, Service};
+use futures::future::Future;
+
+use log::info;
+use env_logger::init;
+
+struct Microservice;
+
+impl Service for Microservice {
+    type Request = Request;
+    type Response = Response;
+    type Error = hyper::Error;
+    type Future = Box<Future<Item = Self::Response, Error = Self::Error>>;
+
+    fn call(&self, request: Request) -> Self::Future {
+        info!("Microservice received a request: {:?}", request);
+        Box::new(futures::future::ok(Response::new()))
+    }
+}
+
