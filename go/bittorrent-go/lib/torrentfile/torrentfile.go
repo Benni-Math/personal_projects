72baff41f1aeb872801d2f38e730a90f306fbbe9
Benni-Math <benediktjens.arnarsson@gmail.com>
2022-07-15 20:07:45 -0400

    Finished coding -- need to test and possibly reformat

--- go/bittorrent-go/lib/torrentfile/torrentfile.go
+++ go/bittorrent-go/lib/torrentfile/torrentfile.go
@@ -39,4 +39,120 @@ type bencodeTorrent struct struct {
 }
 
 // Main function for downloading (calls p2p)
+//  - We first parse the .torrent file into a TorrentFile with Open()
+//  - Then, we call this function to download
+func (t *TorrentFile) DownloadToFile(path string) error {
+    
+    // Setting up our (random) peer ID
+    var peerID [20]byte
+    _, err := rand.Read(peerID[:])
+    if err != nil {
+        return err
+    }
 
+    // Getting our list of peers
+    peers, err := t.requestPeers(peerID, Port)
+    if err != nil {
+        return err
+    }
+
+    // p2p.Torrent is an extention of TorrentFile
+    // which also hold peer information
+    // and which has the main Download() method
+    torrent := p2p.Torrent {
+        Peers:          peers,
+        PeerID:         peerID,
+        InfoHash:       t.InfoHash,
+        PieceHashes:    t.PieceHashes,
+        PieceLength:    t.PieceLength,
+        Length:         t.Length,
+        Name:           t.Name,
+    }
+
+    // Initializing download
+    buf, err := torrent.Download()
+    if err != nil {
+        return err
+    }
+
+    // Writing the contents of the download to a file
+    outFile, err := os.Create(path)
+    if err != nil {
+        return err
+    }
+    defer outFile.Close()
+    _, err = outFile.Write(buf)
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+
+// Parses the .torrent file
+func Open(path string) (TorrentFile, error) {
+    file, err := os.Open(path)
+    if err != nil {
+        return TorrentFile{}, err
+    }
+    defer file.Close()
+
+    bto := bencodeTorrent{}
+    err = bencode.Unmarshal(file, &bto)
+    if err != nil {
+        return TorrentFile{}, err
+    }
+
+    return bto.toTorrentFile()
+}
+
+func (bto *bencodeTorrent) toTorrentFile() (TorrentFile, error) {
+    infoHash, err := bto.Info.hash()
+    if err != nil {
+        return TorrentFile{}, err
+    }
+
+    pieceHashes, err := bto.Info.splitPieceHashes()
+    if err != nil {
+        return TorrentFile{}, err
+    }
+
+    t := TorrentFile {
+        Announce:    bto.Announce,
+        InfoHash:    infoHash,
+        PieceHashes: pieceHashes,
+        PieceLength: bto.Info.PieceLength,
+        Length:      bto.Info.Length,
+        name:        bto.Info.Name,
+    }
+}
+
+func (i *bencodeInfo) hash() ([20]byte, error) {
+    var buf bytes.Buffer
+
+    err := bencode.Marshal(&buf, *i)
+    if err != nil {
+        return [20]byte{}, err
+    }
+
+    h := sha1.Sum(buf.Bytes())
+    return h, nil
+}
+
+func (i *bencodeInfo) splitPieceHashes() ([][20]bytes, error) {
+    hashLen := 20 // Length of SHA-1 hash
+    buf := []byte(i.Pieces)
+    if len(buf)%hashLen != 0 {
+        err := fmt.Errorf("Received malformed pieces of length %d", len(buf))
+        return nil, err
+    }
+
+    numHashes := len(buf) / hashLen
+    hashes := make([][20]byte, numHashes)
+
+    for i := 0; i < numHashes; i++ {
+        copy(hashes[i][:], buf[i*hashLen:(i+1)*hashLen])
+    }
+
+    return hashes, nil
+}
