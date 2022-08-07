380181c4a0633853d593fbf2ffce5402acefa723
Benni-Math <benediktjens.arnarsson@gmail.com>
2022-08-07 10:03:18 -0400

    Basics of POST method working for microservice

--- rust/microservice-rs/src/main.rs
+++ rust/microservice-rs/src/main.rs
@@ -1,22 +1,16 @@
+use std::io;
+use std::collections::HashMap;
+
+use hyper::{Chunk, StatusCode};
+use hyper::Method::{Get, Post};
+use hyper::header::{ContentType, ContentLength};
 use hyper::server::{Request, Response, Service};
-use futures::future::Future;
 
-use log::info;
-use env_logger;
+use futures::Stream;
+use futures::future::{Future, FutureResult};
 
-struct Microservice;
-
-impl Service for Microservice {
-    type Request = Request;
-    type Response = Response;
-    type Error = hyper::Error;
-    type Future = Box<Future<Item = Self::Response, Error = Self::Error>>;
-
-    fn call(&self, request: Request) -> Self::Future {
-        info!("Microservice received a request: {:?}", request);
-        Box::new(futures::future::ok(Response::new()))
-    }
-}
+use serde_json::json;
+use log::{debug, info};
 
 fn main() {
     env_logger::init();
@@ -29,3 +23,87 @@ fn main() {
     server.run().unwrap();
 }
 
+struct Microservice;
+
+impl Service for Microservice {
+    type Request = Request;
+    type Response = Response;
+    type Error = hyper::Error;
+    type Future = Box<dyn Future<Item = Self::Response, Error = Self::Error>>;
+
+    fn call(&self, request: Request) -> Self::Future {
+        match (request.method(), request.path()) {
+            (&Post, "/") => {
+                let future = request
+                    .body()
+                    .concat2()
+                    .and_then(parse_form)
+                    .and_then(write_to_db)
+                    .then(make_post_response);
+                Box::new(future)
+            },
+            _ => Box::new(futures::future::ok(
+                Response::new().with_status(StatusCode::NotFound),
+            )),
+        }
+    }
+}
+
+struct NewMessage {
+    username: String,
+    message: String,
+}
+
+fn parse_form(form_chunk: Chunk) -> FutureResult<NewMessage, hyper::Error> {
+    let mut form = url::form_urlencoded::parse(form_chunk.as_ref())
+        .into_owned()
+        .collect::<HashMap<String, String>>();
+    
+    if let Some(message) = form.remove("message") {
+        let username = form.remove("username")
+            .unwrap_or_else(|| String::from("anonymous"));
+        
+        futures::future::ok(NewMessage {
+            username,
+            message,
+        })
+    } else {
+        futures::future::err(hyper::Error::from(io::Error::new(
+            io::ErrorKind::InvalidInput,
+            "Missing field 'message'",
+        )))
+    }
+}
+
+fn write_to_db(entry: NewMessage) -> FutureResult<i64, hyper::Error> {
+    todo!()
+}
+
+fn make_post_response(
+    result: Result<i64, hyper::Error>,
+) -> FutureResult<hyper::Response, hyper::Error> {
+    match result {
+        Ok(timestamp) => {
+            let payload = json!({"timestamp": timestamp}).to_string();
+            let response = Response::new()
+                .with_header(ContentLength(payload.len() as u64))
+                .with_header(ContentType::json())
+                .with_body(payload);
+            
+            debug!("{:?}", response);
+            futures::future::ok(response)
+        },
+        Err(error) => make_error_response(&error.to_string()),
+    }
+}
+
+fn make_error_response(error_message: &str) -> FutureResult<hyper::Response, hyper::Error> {
+    let payload = json!({"error": error_message}).to_string();
+    let response = Response::new()
+        .with_status(StatusCode::InternalServerError)
+        .with_header(ContentLength(payload.len() as u64))
+        .with_header(ContentType::json())
+        .with_body(payload);
+    debug!("{:?}", response);
+    futures::future::ok(response)
+}
