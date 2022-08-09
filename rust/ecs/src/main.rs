59d433afc0cfe15d1d879cf1e7cb956471af612c
Benni-Math <benediktjens.arnarsson@gmail.com>
2022-08-08 20:25:27 -0400

    Finished small entity-component system walkthrough, now I want to try implementing it for my ABM

--- rust/ecs/src/main.rs
+++ rust/ecs/src/main.rs
@@ -1,3 +1,27 @@
+use std::cell::{RefCell, RefMut};
+struct Health(i32);
+struct Name(&'static str);
+
+trait ComponentVec {
+    fn as_any(&self) -> &dyn std::any::Any;
+    fn as_any_mut(&mut self) -> &mut dyn std::any::Any;
+    fn push_none(&mut self);
+}
+
+impl<T: 'static> ComponentVec for RefCell<Vec<Option<T>>> {
+    fn as_any(&self) -> &dyn std::any::Any {
+        self as &dyn std::any::Any
+    }
+
+    fn as_any_mut(&mut self) -> &mut dyn std::any::Any {
+        self as &mut dyn std::any::Any
+    }
+
+    fn push_none(&mut self) {
+        self.get_mut().push(None)
+    }
+}
+
 struct World {
     // We'll use `entities_count` to assign each Entity a unique ID.
     entities_count: usize,
@@ -32,9 +56,9 @@ impl World {
         for component_vec in self.component_vecs.iter_mut() {
             if let Some(c_v) = component_vec
                 .as_any_mut()
-                .downcast_mut::<Vec<Option<ComponentType>>>() 
+                .downcast_mut::<RefCell<Vec<Option<ComponentType>>>>() 
             {
-                c_v[entity] = Some(component);
+                c_v.borrow_mut()[entity] = Some(component);
                 return;
             }
         }
@@ -48,30 +72,51 @@ impl World {
         }
 
         new_cv[entity] = Some(component);
-        self.component_vecs.push(Box::new(new_cv));
-    }
-}
-
-trait ComponentVec {
-    fn as_any(&self) -> &dyn std::any::Any;
-    fn as_any_mut(&mut self) -> &mut dyn std::any::Any;
-    fn push_none(&mut self);
-}
-
-impl<T> ComponentVec for Vec<Option<T>> {
-    fn as_any(&self) -> &dyn std::any::Any {
-        self as &dyn std::any::Any
+        self.component_vecs.push(Box::new(RefCell::new(new_cv)));
     }
 
-    fn as_any_mut(&mut self) -> &mut dyn std::any::Any {
-        self as &mut dyn std::any::Any
-    }
+    fn borrow_component_vec_mut<ComponentType: 'static>(
+        &self,
+    ) -> Option<RefMut<Vec<Option<ComponentType>>>> {
+        for component_vec in self.component_vecs.iter() {
+            if let Some(component_vec) = component_vec
+                .as_any()
+                .downcast_ref::<RefCell<Vec<Option<ComponentType>>>>()
+            {
+                return Some(component_vec.borrow_mut());
+            }
+        }
 
-    fn push_none(&mut self) {
-        self.push(None)
+        None
     }
 }
 
 fn main() {
-    todo!()
+    let mut world = World::new();
+
+    let icarus_entity = world.new_entity();
+    world.add_component_to_entity(icarus_entity, Name("Icarus"));
+    world.add_component_to_entity(icarus_entity, Health(-10));
+
+    let prometheus_entity = world.new_entity();
+    world.add_component_to_entity(icarus_entity, Name("Prometheus"));
+    world.add_component_to_entity(icarus_entity, Health(100));
+
+    let zeus_entity = world.new_entity();
+    world.add_component_to_entity(zeus_entity, Name("Zeus"));
+
+    let mut healths = world.borrow_component_vec_mut::<Health>().unwrap();
+    let mut names = world.borrow_component_vec_mut::<Name>().unwrap();
+    let zip = healths.iter_mut().zip(names.iter_mut());
+    let iter = zip.filter_map(|(health, name)| Some((health.as_mut()?, name.as_mut()?)));
+
+    for (health, name) in iter {
+        if health.0 < 0 {
+            println!("{} has perished", name.0)
+        }
+
+        if name.0 == "Perseus" && health.0 <= 0 {
+            *health = Health(100);
+        }
+    }
 }
