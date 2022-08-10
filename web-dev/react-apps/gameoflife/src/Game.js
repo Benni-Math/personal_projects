c0315937e6866616b957f7b727cd54f45e34fdb2
Benni-Math <benediktjens.arnarsson@gmail.com>
2022-08-10 14:00:39 -0400

    Almost done, needs some bug-fixing

--- web-dev/react-apps/gameoflife/src/Game.js
+++ web-dev/react-apps/gameoflife/src/Game.js
@@ -17,20 +17,23 @@ class Cell extends React.Component {
           width: `${CELL_SIZE - 1}px`,
           height: `${CELL_SIZE - 1}px`,
         }}
-      >
-
-      </div>
-    )
+      />
+    );
   }
 }
 
 class Game extends React.Component { 
-  state = { cells: [] }
+  state = {
+    cells: [],
+    isRunning: false,
+    interval: 100,
+  }
 
   constructor() {
     super();
     this.rows = HEIGHT / CELL_SIZE;
     this.cols = WIDTH / CELL_SIZE;
+
     this.board = this.makeEmptyBoard();
   }
 
@@ -41,7 +44,7 @@ class Game extends React.Component {
     for (let y = 0; y < this.rows; y++) {
       board[y] = []
       for (let x = 0; x < this.cols; x++) {
-        board[x][y] = false;
+        board[y][x] = false;
       }
     }
 
@@ -80,13 +83,103 @@ class Game extends React.Component {
     const offsetY = event.clientY - elemOffset.y;
 
     const x = Math.floor(offsetX / CELL_SIZE);
-    const y = Math.floor(offsetX / CELL_SIZE);
+    const y = Math.floor(offsetY / CELL_SIZE);
 
     const xIsValid = (x >= 0 && x <= this.cols);
     const yIsValid = (y >= 0 && y<= this.rows);
 
     if (xIsValid && yIsValid) {
       this.board[y][x] = !this.board[y][x];
+    }
+
+    this.setState({ cells: this.makeCells() });
+  }
+
+  runGame = () => {
+    this.setState({ isRunning: true });
+    this.runIteration();
+  }
+
+  stopGame = () => {
+    this.setState({ isRunning: false });
+
+    if (this.timeoutHandler) {
+      window.clearTimeout(this.timeoutHandler);
+      this.timeoutHandler = null;
+    }
+  }
+
+  runIteration() {
+    let newBoard = this.makeEmptyBoard;
+
+    for (let y = 0; y < this.rows; y++) {
+      for (let x = 0; x < this.cols; x++) {
+        let neighbors = this.calculateNeighbors(this.board, x, y);
+        if (this.board[y][x]) {
+          if (neighbors === 2 || neighbors === 3) {
+            newBoard[y][x] = true;
+          } else {
+            newBoard[y][x] = false;
+          }
+        } else {
+          if (!this.board[y][x] && neighbors === 3) {
+            newBoard[y][x] = true;
+          }
+        }
+      }
+    }
+
+    this.board = newBoard;
+    this.setState({ cells: this.makeCells() });
+
+    this.timeoutHandler = window.setTimeout(() => {
+      this.runIteration()
+    }, this.state.interval);
+  }
+
+  calculateNeighbors(board, x, y) {
+    let neighbors = 0;
+
+    const dirs =[
+      [-1, -1],
+      [-1, 0],
+      [-1, 1],
+      [0, 1],
+      [1, 1],
+      [1, 0],
+      [1, -1],
+      [0, -1],
+    ];
+
+    for (const dir of dirs) {
+      const x1 = x + dir[1];
+      const x1IsValid = (x1 >= 0 && x1 <= this.cols);
+
+      const y1 = y + dir[0];
+      const y1IsValid = (y1 >= 0 && y1 <= this.rows);
+
+      if (x1IsValid && y1IsValid && board[y1][x1]) {
+        neighbors++;
+      }
+    }
+
+    return neighbors;
+  }
+
+  handleIntervalChange = (event) => {
+    this.setState({ interval: event.target.value });
+  }
+
+  handleClear = () => {
+    this.board = this.makeEmptyBoard();
+    this.setState({ cells: this.makeCells() });
+  }
+
+  handleRandom = () => {
+    for (let y = 0; y < this.rows; y++) {
+      for (let x = 0; x < this.cols; x++) {
+        this.board[y][x] = (Math.random() >= 0.5);
+      }
     }
 
     this.setState({ cells: this.makeCells() });
@@ -104,13 +197,19 @@ class Game extends React.Component {
           ref={(n) => { this.boardRef = n; }}
         >
           {cells.map(cell => (
-            <Cell
-              x={cell.x} 
-              y={cell.y}
-              key={`${cell.x},${cell.y}`}
-            />
+            <Cell x={cell.x} y={cell.y} key={`${cell.x},${cell.y}`} />
           ))}
         </div>
+
+        <div className="controls">
+                    Update every <input value={this.state.interval} onChange={this.handleIntervalChange} /> msec
+                    {this.state.isRunning ?
+                        <button className="button" onClick={this.stopGame}>Stop</button> :
+                        <button className="button" onClick={this.runGame}>Run</button>
+                    }
+                    <button className="button" onClick={this.handleRandom}>Random</button>
+                    <button className="button" onClick={this.handleClear}>Clear</button>
+                </div>
       </div>
     )
   }
